0084b0b59a60947c579afa2d2a38b999e6a32d32
Anjal Binayak <anjalbinayak3@gmail.com>
2022-02-25 21:55:55 +0545

    List of Tools Updated

--- pages/tools/index.js
+++ pages/tools/index.js
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
 
@@ -8,16 +8,27 @@ export default function Index() {
       <Head>
         <title>All Tools - Dev Tools</title>
       </Head>
-      <h1 className="text-lg text-center">Developer Tools</h1>
+      <h1 className="text-lg text-center">List of Available Tools</h1>
       <Box className="m-2 w-auto">
-        <Link href="/tools/image" passHref>
-          <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-800">
-            <span className="mr-2">Image to Base64 Converter</span>
-            <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
-              9
-            </span>
-          </button>
-        </Link>
+        <Stack direction={"row"} spacing="2">
+          <Link href="/tools/image" passHref>
+            <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-800">
+              <span className="mr-2">Image to Base64 Converter</span>
+            </button>
+          </Link>
+
+          <Link href="/tools/url-encode" passHref>
+            <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 bg-blue-500 rounded-lg focus:shadow-outline hover:bg-blue-700">
+              <span className="mr-2">URL Encoder</span>
+            </button>
+          </Link>
+
+          <Link href="/tools/url-decode" passHref>
+            <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 bg-gray-500 rounded-lg focus:shadow-outline hover:bg-gray-700">
+              <span className="mr-2">URL Decoder</span>
+            </button>
+          </Link>
+        </Stack>
       </Box>
     </>
   );
