a2b720879c83e43f8fb2a1e7e85ebea14d1e6b4f
Anjal Binayak <anjalbinayak3@gmail.com>
2022-02-26 09:38:27 +0545

    responsiveness added to list of tools

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Index() {
       </Head>
       <h1 className="text-lg text-center">List of Available Tools</h1>
       <Box className="m-2 w-auto">
-        <Stack direction={"row"} spacing="2">
+        <Stack direction={["column", "column", "row"]} spacing="2">
           <Link href="/tools/image" passHref>
             <button className="h-10 px-5 text-indigo-100 transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-800">
               <span className="mr-2">Image to Base64 Converter</span>
